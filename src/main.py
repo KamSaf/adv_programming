2efc19f17a646314a8db0f636da69695cf9fad9b
KamSaf <bopaf42@gmail.com>
2024-12-02 17:36:23 +0100

    Delete image conversion before text recocgnition

--- src/main.py
+++ src/main.py
@@ -8,9 +8,7 @@ FILES = ["image_1.png", "image_2.jpg", "image_3.jpg", "image_4.jpg", "image_5.pn
 
 def read_text(file_path: str) -> str:
     img = cv2.imread(file_path)
-    conv_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    conv_img = cv2.medianBlur(img, 3)
-    return pytesseract.image_to_string(conv_img)
+    return pytesseract.image_to_string(img)
 
 
 if __name__ == "__main__":
