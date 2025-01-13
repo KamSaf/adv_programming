644913ecb920635fd4c95a4bfab1f9022368dd26
KamSaf <bopaf42@gmail.com>
2025-01-13 18:29:44 +0100

    Create bash scripts for starting and stopping consumers

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PYTHON_SCRIPT="src/consumer.py"
+NUM_PROCESSES=3
+DELAY=10
+
+for ((i=1; i<=NUM_PROCESSES; i++)); do
+    echo "Starting consumer $i..."
+    python3 "$PYTHON_SCRIPT" --id=$i &
+    sleep $DELAY
+done
+
+echo "All consumers have been started."
